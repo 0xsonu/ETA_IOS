60465e63def72c1eda354ba7171c2c838d634e0a
Sonu Kumar <sonu.patna0808@gmail.com>
2023-06-19 11:33:38 +0530

    Initial Commit

---
+++ ETA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ETA
+//
+//  Created by Sonu Kumar on 19/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ETA/ETAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETAApp.swift
+//  ETA
+//
+//  Created by Sonu Kumar on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ETAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
